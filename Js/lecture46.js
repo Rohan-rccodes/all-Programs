61efb954e41b5813aa8310721fb928681f61ac9b
rccdoes <itschauhan101@gmail.com>
2025-03-27 21:14:10 +0530

    new lecture

---
+++ Js/lecture46.js
@@ -0,0 +1,5 @@
+//Temporal dead zone
+//let and const are hoisted but not initialized
+//console.log(num); //ReferenceError: Cannot access 'num' before initialization
+let num = 10;
+console.log(num); //10
