d8403eda0ae1dd8d2f67959af609562fa6763543
rccdoes <itschauhan101@gmail.com>
2025-03-28 20:57:14 +0530

    new

---
+++ Js/lec472.js
@@ -0,0 +1,19 @@
+// default parameters
+
+// function sayhello(name) {
+//   name = name.toUpperCase();
+//   console.log("Hello " + name);
+// }
+// sayhello("Rahul");
+
+function solve(
+  value = {age: 30,wt:700, ht: 33 }) {
+
+
+  console.log("hello jiii ", value);
+  console.log("hello jiii ", value.age);
+  console.log("hello jiii ", value.wt);
+  console.log("hello jiii ", value.ht);
+}
+solve();
+  
