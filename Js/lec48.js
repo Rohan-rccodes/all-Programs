1703a1e76169709611c48733bcf7d3c11a5143f9
rccdoes <itschauhan101@gmail.com>
2025-03-28 21:04:47 +0530

    built in objects

---
+++ Js/lec48.js
@@ -0,0 +1,34 @@
+// common built in objects in js
+console.log('Welcome to tutorial 48');
+
+// maths object
+
+console.log(Math.E);
+console.log(Math.PI);
+
+//change negative to positive number 
+console.log(Math.abs(-5));
+
+//maximum number
+console.log(Math.max(5, 10, 20, 30, 40, 50));
+
+//minimum number        
+console.log(Math.min(5, 10, 20, 30, 40, 50));
+
+//square root   
+console.log(Math.sqrt(64));
+
+//power of a number 
+console.log(Math.pow(2, 3));
+
+//round off
+console.log(Math.round(5.4));
+
+//round off to upper value
+console.log(Math.ceil(5.4));
+
+//round off to lower value      
+console.log(Math.floor(5.4));
+
+//random number from 0 to 1
+console.log(Math.random());
