b10446903a8ee89f3cdd6bf64944f2faa0a0cfdb
rccdoes <itschauhan101@gmail.com>
2025-03-28 21:10:43 +0530

    new date

--- Js/lec48.js
+++ Js/lec48.js
@@ -3,32 +3,45 @@ console.log('Welcome to tutorial 48');
 
 // maths object
 
-console.log(Math.E);
-console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.PI);
 
-//change negative to positive number 
-console.log(Math.abs(-5));
+// //change negative to positive number 
+// console.log(Math.abs(-5));
 
-//maximum number
-console.log(Math.max(5, 10, 20, 30, 40, 50));
+// //maximum number
+// console.log(Math.max(5, 10, 20, 30, 40, 50));
 
-//minimum number        
-console.log(Math.min(5, 10, 20, 30, 40, 50));
+// //minimum number        
+// console.log(Math.min(5, 10, 20, 30, 40, 50));
 
-//square root   
-console.log(Math.sqrt(64));
+// //square root   
+// console.log(Math.sqrt(64));
 
-//power of a number 
-console.log(Math.pow(2, 3));
+// //power of a number 
+// console.log(Math.pow(2, 3));
 
-//round off
-console.log(Math.round(5.4));
+// //round off
+// console.log(Math.round(5.4));
 
-//round off to upper value
-console.log(Math.ceil(5.4));
+// //round off to upper value
+// console.log(Math.ceil(5.4));
 
-//round off to lower value      
-console.log(Math.floor(5.4));
+// //round off to lower value      
+// console.log(Math.floor(5.4));
 
-//random number from 0 to 1
-console.log(Math.random());
+// //random number from 0 to 1
+// console.log(Math.random());
+
+// //random number from 0 to 10    
+// console.log(Math.random() * 10);
+
+// //random number from 0 to 10 and round off
+// console.log(Math.floor(Math.random() * 10));
+
+let curr = new Date();
+
+let date = new Date( 'June 20 2021 11:30:59');
+
+console.log(date);
+console.log(curr);
