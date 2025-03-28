9a0495a08dca4f239359d99b59134e39fc2c2f6e
rccdoes <itschauhan101@gmail.com>
2025-03-28 17:11:54 +0530

    variable scope

--- Js/lecture46.js
+++ Js/lecture46.js
@@ -2,4 +2,22 @@
 //let and const are hoisted but not initialized
 //console.log(num); //ReferenceError: Cannot access 'num' before initialization
 let num = 10;
-console.log(num); //10
+      console.log(num); //10
+
+      {
+        console.log(num); //10
+        
+      }
+
+      if(true){
+        console.log("called 3 time" , num); //10
+      }
+
+      for(let i = 0; i < 5; i++){
+        console.log(num); //0 1 2 3 4
+      }
+
+      function test(){
+        console.log(num); //10
+      }
+      test();
