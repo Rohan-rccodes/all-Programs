2f739b5b084633fd0cfe7a6ab82043f8d0a29c5f
rccdoes <itschauhan101@gmail.com>
2025-03-29 00:37:23 +0530

    try catch

---
+++ Js/lec50.js
@@ -0,0 +1,14 @@
+// Error handling 
+
+console.log("Welcome to tutorial 50");
+try{
+
+console.log("try block executed");
+console.log(x);
+
+console.log("try block ends here");
+}
+catch(e){
+    console.log("catch block executed");
+   
+}
